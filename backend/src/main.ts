cf0edfc32d07dc3f18938c53b568678eac43deec
Haltarys <gregoire.haardt@gmail.com>
2021-01-15 17:50:57 +0100

    build(backend): dockerise backend application

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PORT } from './config/config';
 
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT);
 };
 
 bootstrap();
