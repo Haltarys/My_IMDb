d0e6eab105387bd5b441d6210a7bb70bf43a1da0
Haltarys <gregoire.haardt@gmail.com>
2021-02-03 16:51:06 +0100

    feat(backend): add Universe schema and service for MongoDB

---
+++ backend/src/graphql/universe/universe.schema.ts
@@ -0,0 +1,20 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, ObjectId, Types } from 'mongoose';
+
+export type UniverseDocument = Universe & Document;
+
+@Schema()
+export class Universe {
+  @Prop({
+    required: true,
+  })
+  name: string;
+
+  @Prop({
+    type: [Types.ObjectId],
+    required: true,
+  })
+  films: ObjectId[];
+}
+
+export const UniverseSchema = SchemaFactory.createForClass(Universe);
---
+++ backend/src/graphql/universe/universe.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, ObjectId } from 'mongoose';
+import { Universe, UniverseDocument } from './universe.schema';
+import { IDType } from '../id-type';
+
+@Injectable()
+export class UniverseService {
+  constructor(
+    @InjectModel(Universe.name) private universeModel: Model<UniverseDocument>,
+  ) {}
+
+  async findAllUniverses(): Promise<Universe[]> {
+    const universes = await this.universeModel.find().exec();
+
+    return universes;
+  }
+
+  async findUniverseByID(id: IDType | ObjectId): Promise<Universe> {
+    const universe = await this.universeModel.findById(id).exec();
+
+    return universe;
+  }
+
+  async findUniverseByName(name: string): Promise<Universe> {
+    const universe = await this.universeModel.findOne({ name }).exec();
+
+    return universe;
+  }
+
+  async findUniversesWithIDs(
+    universeIDs: IDType[] | ObjectId[],
+  ): Promise<Universe[]> {
+    // Currently, there is an issue with Typescript on the .map() method
+    // with union or array types (see: https://github.com/microsoft/TypeScript/issues/36390)
+    // The workaround is to cast the array of IDs to 'any[]' to use .map()
+
+    const universes = await this.universeModel
+      .find({ _id: { $in: universeIDs } })
+      .exec()
+      .then((unorderedUniverses) =>
+        (universeIDs as any[]).map((id) =>
+          unorderedUniverses.find((universe) => universe.id === String(id)),
+        ),
+      );
+
+    return universes;
+  }
+}
