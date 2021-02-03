59fc0f00de0628e40948413eea97b211506bb3fe
Haltarys <gregoire.haardt@gmail.com>
2021-02-03 16:48:08 +0100

    feat(backend): add Film schema and service for MongoDB

---
+++ backend/src/graphql/media/film/film.schema.ts
@@ -0,0 +1,70 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, ObjectId, Types } from 'mongoose';
+
+export type FilmDocument = Film & Document;
+
+@Schema()
+export class Film {
+  @Prop({
+    required: true,
+  })
+  title: string;
+
+  @Prop({
+    required: true,
+  })
+  year: number;
+
+  @Prop({
+    required: true,
+  })
+  genres: string[];
+
+  @Prop({
+    required: true,
+  })
+  runningTime: number;
+
+  @Prop()
+  basedOnTrueFacts?: boolean;
+
+  @Prop({
+    type: Types.ObjectId,
+  })
+  basedOnBook?: ObjectId;
+
+  @Prop({
+    type: [Types.ObjectId],
+    required: true,
+  })
+  directedBy: ObjectId[];
+
+  @Prop({
+    type: [Types.ObjectId],
+    required: true,
+  })
+  musicBy: ObjectId[];
+
+  @Prop({
+    type: [Types.ObjectId],
+    required: true,
+  })
+  cast: ObjectId[];
+
+  @Prop({
+    type: Types.ObjectId,
+  })
+  previous?: ObjectId;
+
+  @Prop({
+    type: Types.ObjectId,
+  })
+  sequel?: ObjectId;
+
+  @Prop({
+    type: Types.ObjectId,
+  })
+  cinematicUniverse?: ObjectId;
+}
+
+export const FilmSchema = SchemaFactory.createForClass(Film);
---
+++ backend/src/graphql/media/film/film.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, ObjectId } from 'mongoose';
+import { Film, FilmDocument } from './film.schema';
+import { IDType } from 'src/graphql/id-type';
+
+@Injectable()
+export class FilmService {
+  constructor(@InjectModel(Film.name) private filmModel: Model<FilmDocument>) {}
+
+  async findAllFilms(): Promise<Film[]> {
+    const films = await this.filmModel.find().exec();
+
+    return films;
+  }
+
+  async findFilmByID(id: IDType | ObjectId): Promise<Film> {
+    const film = await this.filmModel.findById(id).exec();
+
+    return film;
+  }
+
+  async findFilmByTitle(title: string): Promise<Film> {
+    const film = await this.filmModel.findOne({ title }).exec();
+
+    return film;
+  }
+
+  async findFilmsWithIDs(filmIDs: IDType[] | ObjectId[]): Promise<Film[]> {
+    // Currently, there is an issue with Typescript on the .map() method
+    // with union or array types (see: https://github.com/microsoft/TypeScript/issues/36390)
+    // The workaround is to cast the array of IDs to 'any[]' to use .map()
+
+    const films = await this.filmModel
+      .find({ _id: { $in: filmIDs } })
+      .exec()
+      .then((unorderedFilms) =>
+        (filmIDs as any[]).map((id) =>
+          unorderedFilms.find((film) => film.id === String(id)),
+        ),
+      );
+
+    return films;
+  }
+}
