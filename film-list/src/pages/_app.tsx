e6d9a28e87da1bdb58258f9387752968608b25b5
Haltarys <gregoire.haardt@gmail.com>
2021-05-17 21:28:02 +0200

    refactor(film-list): refactor <head> code and components & hooks

--- film-list/src/pages/_app.tsx
+++ film-list/src/pages/_app.tsx
@@ -1,40 +1,42 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import { AppProps } from 'next/app';
+import { useEffect } from 'react';
 import Head from 'next/head';
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import theme from '../theme/theme';
+import theme from '@/theme/theme';
 
-export default function MyApp(props) {
+const MyApp = (props: AppProps) => {
   const { Component, pageProps } = props;
 
-  React.useEffect(() => {
+  useEffect(() => {
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side');
     if (jssStyles) {
-      jssStyles.parentElement.removeChild(jssStyles);
+      jssStyles.parentElement?.removeChild(jssStyles);
     }
-  }, []);
+  }, []); // Only for initial render
 
   return (
-    <React.Fragment>
+    <>
       <Head>
-        <title>My page</title>
+        <title>My IMDb film list</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
+        <meta
+          name="description"
+          content="A list of film that I intend to watch some day (and add to My IMDb)."
+        />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         <Component {...pageProps} />
       </ThemeProvider>
-    </React.Fragment>
+    </>
   );
-}
-
-MyApp.propTypes = {
-  Component: PropTypes.elementType.isRequired,
-  pageProps: PropTypes.object.isRequired,
 };
+
+export default MyApp;
